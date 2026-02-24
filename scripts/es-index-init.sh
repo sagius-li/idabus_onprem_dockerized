316463faeb5c13deaeb0a1b20524d2043db18399
Jie Li <jl@ocg.de>
2026-02-24 23:53:25 +0100

    formated es-index-init.sh

--- scripts/es-index-init.sh
+++ scripts/es-index-init.sh
@@ -16,55 +16,55 @@ for index in events eventsarchive resources workflowexecution; do
       -X PUT "http://elasticsearch:9200/$index" \
       -H "Content-Type: application/json" \
       --data-binary @- <<'JSON'
-{
-  "settings": {
-    "number_of_shards": 1,
-    "index.mapping.ignore_malformed": true,
-    "index.mapping.total_fields.limit": 2500,
-    "analysis": {
-      "normalizer": {
-        "string_normalizer": {
-          "type": "custom",
-          "char_filter": [],
-          "filter": ["lowercase", "asciifolding"]
-        }
-      }
-    }
-  },
-  "mappings": {
-    "date_detection": false,
-    "dynamic_templates": [
-      {
-        "ignore_x_fields": {
-          "path_match": "x_*",
-          "mapping": {"type": "object", "enabled": false}
-        }
-      },
-      {
-        "ignore_nested_x_fields": {
-          "path_match": "*.x_*",
-          "mapping": {"type": "object", "enabled": false}
-        }
-      },
-      {
-        "string_fields": {
-          "match": "s_*",
-          "mapping": {
-            "type": "keyword",
-            "normalizer": "string_normalizer",
-            "ignore_above": 1024
+        {
+          "settings": {
+            "number_of_shards": 1,
+            "index.mapping.ignore_malformed": true,
+            "index.mapping.total_fields.limit": 2500,
+            "analysis": {
+              "normalizer": {
+                "string_normalizer": {
+                  "type": "custom",
+                  "char_filter": [],
+                  "filter": ["lowercase", "asciifolding"]
+                }
+              }
+            }
+          },
+          "mappings": {
+            "date_detection": false,
+            "dynamic_templates": [
+              {
+                "ignore_x_fields": {
+                  "path_match": "x_*",
+                  "mapping": {"type": "object", "enabled": false}
+                }
+              },
+              {
+                "ignore_nested_x_fields": {
+                  "path_match": "*.x_*",
+                  "mapping": {"type": "object", "enabled": false}
+                }
+              },
+              {
+                "string_fields": {
+                  "match": "s_*",
+                  "mapping": {
+                    "type": "keyword",
+                    "normalizer": "string_normalizer",
+                    "ignore_above": 1024
+                  }
+                }
+              },
+              {
+                "all_other_strings": {
+                  "match_mapping_type": "string",
+                  "mapping": {"type": "keyword", "ignore_above": 1024}
+                }
+              }
+            ]
           }
         }
-      },
-      {
-        "all_other_strings": {
-          "match_mapping_type": "string",
-          "mapping": {"type": "keyword", "ignore_above": 1024}
-        }
-      }
-    ]
-  }
-}
 JSON
 )
     if [ "$create_status" != "200" ] && [ "$create_status" != "201" ]; then
