7a6775ae1460042e380cc1c1ceced555801f266a
Jie Li <jl@ocg.de>
2026-02-26 20:15:09 +0100

    updated sqlserver-init.sh

--- scripts/sqlserver-init.sh
+++ scripts/sqlserver-init.sh
@@ -12,7 +12,7 @@ SLEEP_SECONDS="${SLEEP_SECONDS:-2}"
 
 echo "Waiting for SQL Server at ${SQLSERVER_HOST}:${SQLSERVER_PORT} to accept connections..."
 retry=1
-until "$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -Q "SELECT 1" >/dev/null 2>&1; do
+until "$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -b -Q "SELECT 1" >/dev/null 2>&1; do
   if [ "$retry" -ge "$MAX_RETRIES" ]; then
     echo "SQL Server did not become ready after $((MAX_RETRIES * SLEEP_SECONDS)) seconds." >&2
     exit 1
@@ -23,9 +23,9 @@ until "$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$
 done
 
 echo "Ensuring database [$DB_NAME] exists..."
-"$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -Q "IF DB_ID(N'$DB_NAME') IS NULL CREATE DATABASE [$DB_NAME];"
+"$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -b -Q "IF DB_ID(N'$DB_NAME') IS NULL CREATE DATABASE [$DB_NAME];"
 
-DB_EXISTS=$("$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -h -1 -W -Q "SET NOCOUNT ON; SELECT CASE WHEN DB_ID(N'$DB_NAME') IS NULL THEN 0 ELSE 1 END;")
+DB_EXISTS=$("$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -b -h -1 -W -Q "SET NOCOUNT ON; SELECT CASE WHEN DB_ID(N'$DB_NAME') IS NULL THEN 0 ELSE 1 END;")
 if [ "$DB_EXISTS" != "1" ]; then
   echo "Failed to ensure database [$DB_NAME]." >&2
   exit 1
@@ -33,12 +33,19 @@ fi
 echo "Database [$DB_NAME] is ready."
 
 echo "Ensuring READ_COMMITTED_SNAPSHOT is enabled..."
-"$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -Q "IF EXISTS (SELECT 1 FROM sys.databases WHERE name = N'$DB_NAME' AND is_read_committed_snapshot_on = 0) ALTER DATABASE [$DB_NAME] SET READ_COMMITTED_SNAPSHOT ON;"
+"$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -b -Q "IF EXISTS (SELECT 1 FROM sys.databases WHERE name = N'$DB_NAME' AND is_read_committed_snapshot_on = 0) ALTER DATABASE [$DB_NAME] SET READ_COMMITTED_SNAPSHOT ON;"
 
 for table_name in $TABLE_NAMES; do
   echo "Ensuring table [$table_name] and indexes exist..."
-  "$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -Q "
+  "$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -b -Q "
 USE [$DB_NAME];
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+SET ANSI_PADDING ON;
+SET ANSI_WARNINGS ON;
+SET CONCAT_NULL_YIELDS_NULL ON;
+SET ARITHABORT ON;
+SET NUMERIC_ROUNDABORT OFF;
 IF OBJECT_ID(N'dbo.[$table_name]', N'U') IS NULL
 BEGIN
   CREATE TABLE [dbo].[$table_name]
@@ -47,32 +54,32 @@ BEGIN
     [ObjectId] UNIQUEIDENTIFIER NOT NULL,
     [LastUpdateTime] BIGINT NOT NULL,
     [JsonData] NVARCHAR(MAX) NOT NULL,
+    [IsDeleted] BIT NOT NULL CONSTRAINT [DF_${table_name}_IsDeleted] DEFAULT ((0)),
     CONSTRAINT [PK_${table_name}] PRIMARY KEY CLUSTERED ([Id])
   );
-END;
-
-IF NOT EXISTS (
-  SELECT 1
-  FROM sys.indexes
-  WHERE object_id = OBJECT_ID(N'dbo.[$table_name]')
-    AND name = N'UQ_${table_name}_ObjectId'
-)
-BEGIN
   CREATE UNIQUE NONCLUSTERED INDEX [UQ_${table_name}_ObjectId]
-    ON [dbo].[$table_name] ([ObjectId]);
-END;
-
-IF NOT EXISTS (
-  SELECT 1
-  FROM sys.indexes
-  WHERE object_id = OBJECT_ID(N'dbo.[$table_name]')
-    AND name = N'IX_${table_name}_LastUpdateTime'
-)
-BEGIN
-  CREATE NONCLUSTERED INDEX [IX_${table_name}_LastUpdateTime]
-    ON [dbo].[$table_name] ([LastUpdateTime]);
+    ON [dbo].[$table_name] ([ObjectId])
+    WHERE [IsDeleted] = 0;
+  CREATE NONCLUSTERED INDEX [IX_${table_name}_IsDeleted_LastUpdateTime]
+    ON [dbo].[$table_name] ([IsDeleted], [LastUpdateTime]);
 END;
 "
 done
+
+echo "Ensuring table [Metadata] exists..."
+"$SQLCMD_BIN" -S "${SQLSERVER_HOST},${SQLSERVER_PORT}" -U "$SA_USER" -P "$SA_PASSWORD" -C -b -Q "
+USE [$DB_NAME];
+IF OBJECT_ID(N'dbo.[Metadata]', N'U') IS NULL
+BEGIN
+  CREATE TABLE [dbo].[Metadata]
+  (
+    [EntryKey] NVARCHAR(128) NOT NULL,
+    [LastSuccessfulFlushUtc] BIGINT NOT NULL,
+    [FailedItemsJson] NVARCHAR(MAX) NOT NULL,
+    [UpdatedUtc] BIGINT NOT NULL,
+    CONSTRAINT [PK_Metadata] PRIMARY KEY CLUSTERED ([EntryKey])
+  );
+END;
+"
 
 echo "SQL bootstrap completed successfully."
